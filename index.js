2fa685f8339324a908c78c8ba5aadf74e0970903
Karissa McKelvey <krmckelv@gmail.com>
2015-04-23 16:53:24 -0700

    create daff-stream module'

--- index.js
+++ index.js
@@ -1,64 +1,43 @@
-var daff = require('daff')
-var batcher = require('byte-stream')
-var through = require('through2')
+var Transform = require('stream').Transform
+var inherits = require('inherits')
+var debug = require('debug')('daff-stream')
+var diff2daff = require('./diff2daff.js')
 
-module.exports = NdjsonStream
+module.exports = DaffStream
 
-function NdjsonStream (stream1, stream2, opts, cb) {
-  // takes two ndjson streams
-  // TODO: modularize into daff's core
-  // returns cb(table1, table2, visual, next)
+inherits(DaffStream, Transform)
+function DaffStream (rowPath) {
+  /*
+  transforms to:
+    {
+      tables: daff tables,
+      visual: the terminal visual for that daff
+    }
+  */
+  if (!(this instanceof DaffStream)) return new DaffStream(rowPath)
+  Transform.call(this, {objectMode: true})
+  this.destroyed = false
+  this.rowPath = rowPath
+}
 
-  if (!opts) opts = {}
-  if (!opts.limit) opts.limit = 20
-  if (!cb) throw new Error('no callback')
-
-  stream1 = createBatchedStream(stream1, opts.limit)
-  stream2 = createBatchedStream(stream2, opts.limit)
-
-  batchedStreamToTable(stream1, function (table1, next1) {
-    batchedStreamToTable(stream2, function (table2, next2) {
-      var tables = [table1, table2]
-      var visual = tablesToVisual(tables, opts)
-
-      cb(tables, visual, function next (cb) {
-        next1()
-        next2()
-      })
-    })
+DaffStream.prototype._transform = function (data, enc, next) {
+  var self = this
+  debug('_transform', data)
+  var opts = {
+    rowPath: self.rowPath
+  }
+  diff2daff(data, opts, function (tables, visual) {
+    var output = {
+      tables: tables,
+      visual: visual
+    }
+    next(null, output)
   })
 }
 
-function createBatchedStream (stream, limit) {
-  var batchedStream = batcher(limit)
-  return stream.pipe(batchedStream)
-}
-
-function batchedStreamToTable (stream, cb) {
-  stream.pipe(through.obj(function (data, enc, next) {
-    var table = new daff.NdjsonTable(data)
-    cb(table, next)
-  }))
-}
-
-function tablesToVisual (tables, opts) {
-  var flags = new daff.CompareFlags()
-
-  var table1 = tables[0]
-  var table2 = tables[1]
-
-  var alignment = daff.compareTables(table1, table2, flags).align()
-  var highlighter = new daff.TableDiff(alignment, flags)
-  var table_diff = new daff.SimpleTable()
-  highlighter.hilite(table_diff)
-
-  if (opts.html) {
-    var diff2html = new daff.DiffRender()
-    diff2html.render(table_diff)
-    var table_diff_html = diff2html.html()
-    return table_diff_html
-  } else {
-    var diff2terminal = new daff.TerminalDiffRender()
-    return diff2terminal.render(table_diff)
-  }
+DaffStream.prototype.destroy = function (err) {
+  if (this.destroyed) return
+  this.destroyed = true
+  this.err = err
+  this.end()
 }
--- test.js
+++ test.js
@@ -1,50 +1,49 @@
 var test = require('tape')
+var diff = require('sorted-diff-stream')
 var from = require('from2')
-
-var daffStream = require('./index.js')
+var through = require('through2')
+var batcher = require('byte-stream')
 var DATA = require('test-data')
+
+var daffStream = require('./')
 
 var TABLES = DATA.CONFLICTS.SMALL
 
-test('dat2daff.fromReadStreams', function (t) {
-  var stream1 = from.obj(TABLES[1])
-  var stream2 = from.obj(TABLES[2])
-  var opts = {
-    html: false,
-    limit: 20
+test('knead from sorted-diff-stream', function (t) {
+  function keyData (data) {
+    var index = 0
+    data.map(function (obj) {
+      var rObj = {}
+      rObj.key = index
+      rObj.value = obj
+      index++
+      return rObj
+    })
+    return data
   }
-  daffStream(stream1, stream2, opts, function (tables, visual, next) {
-    var table1 = tables[0]
-    var table2 = tables[1]
-    t.equals(table1.height, 5)
-    t.equals(table2.height, 12)
-    t.deepEquals(table1.columns, ['capital', 'country'])
-    t.deepEquals(table2.columns, ['capital', 'code', 'country'])
-    t.same(typeof visual, 'string')
-    t.same(typeof next, 'function')
-    console.log(visual)
-    t.end()
-  })
-})
 
-test('dat2daff.fromReadStreams with small limit', function (t) {
-  var stream1 = from.obj(TABLES[1])
-  var stream2 = from.obj(TABLES[2])
-  var opts = {
-    html: false,
-    limit: 5
+  var older = from.obj(keyData(TABLES[1]))
+  var newer = from.obj(keyData(TABLES[2]))
+
+  function jsonEquals (a, b, cb) {
+    if (JSON.stringify(a) === JSON.stringify(b)) cb(null, true)
+    else cb(null, false)
   }
-  daffStream(stream1, stream2, opts, function (tables, visual, next) {
-    var table1 = tables[0]
-    var table2 = tables[1]
-    t.equals(table1.height, 5)
-    t.equals(table2.height, 12)
-    t.deepEquals(table1.columns, ['capital', 'country'])
-    t.deepEquals(table2.columns, ['capital', 'code', 'country'])
-    t.same(typeof visual, 'string')
-    t.same(typeof next, 'function')
-    console.log(tables)
-    console.log(visual)
-    t.end()
-  })
+
+  var diffStream = diff(older, newer, jsonEquals)
+  diffStream.pipe(batcher(3*2)).pipe(daffStream()).pipe(
+    through.obj(function (data, enc, next) {
+      var table1 = data.tables[0]
+      var table2 = data.tables[1]
+      console.log(data.visual)
+
+      t.equals(table1.height, 4)
+      t.equals(table2.height, 4)
+      t.deepEquals(table1.columns, ['capital', 'country'])
+      t.deepEquals(table2.columns, ['capital', 'code', 'country'])
+      t.same(typeof data.visual, 'string')
+      t.same(typeof next, 'function')
+      t.end()
+    })
+  )
 })
